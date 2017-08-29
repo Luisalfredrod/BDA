a94171ebe13b4432df47b4d3b3d97b59bdb1bc09
EduardoVaca <vacalalo@gmail.com>
2017-08-29 12:45:54 -0500

    Generating cities

--- create_data.py
+++ create_data.py
@@ -1,7 +1,35 @@
+CONTINENT_COUNTRY = {
+    'Europe': [
+            'Austria', 'Belgium', 'Denmark', 'Finland', 'France', 'Germany',
+            'Italy', 'Netherlands', 'Norway', 'Spain', 'Sweden', 'UK'
+        ],
+    'America': [
+        'Argentina', 'Brazil', 'Canada', 'Chile', 'Colombia', 'Mexico', 'Uruguay', 'USA', 
+    ],
+    'Asia': [
+        'China', 'India', 'Japan', 'Russia', 'Singapore',
+        'South Korea', 'Thailand', 'United Arab Emirates', 'Vietnam'
+    ],
+    'Oceania': [
+        'Australia', 'Fiji', 'New Zealand'
+    ]
+}
 
+COUNTRY_CITY = {
+    'Austria': ['Vienna'], 'Belgium': ['Brussels'], 'Denmark': ['Copenhagen'], 'Finland': ['Helsinki'],
+    'France': ['Paris', 'Lyon'], 'Germany': ['Frankfurt', 'Hambourg'], 'Italy': ['Rome', 'Florence'],
+    'Netherlands': ['Amsterdam'], 'Norway': ['Oslo'], 'Spain': ['Madrid'], 'Sweden': ['Stockholm'], 'UK': ['London'],
+    'Argentina': ['Buenos Aires'], 'Brazil': ['Sao Paolo'], 'Canada': ['Vancouver', 'Quebec', 'Alberta'], 
+    'Chile': ['Santiago'], 'Colombia': ['Medellin'], 'Mexico': ['Mexico City', 'Monterrey', 'Queretaro'],
+    'Uruguay': ['Montevideo'], 'USA': ['San Francisco', 'New York', 'Seattle', 'Miami', 'Chicago'],
+    'China': ['Hong Kong', 'Beijing'], 'India': ['Mumbai'], 'Japan': ['Tokio'], 'Russia': ['Moscow'],
+    'Singapore': ['Tampines'], 'South Korea': ['Seul'], 'Thailand': ['Bangkok'], 'United Arab Emirates': ['Dubai'],
+    'Vietnam': ['Hanoi'], 'Australia': ['Melbourne', 'Sydney'], 'Fiji': ['Suva'], 'New Zealand': ['Wellignton']
+}
 
 
 def create_continent_data():
+    print('Creating continents...')
     continent_file = open("continents.csv", "w")
     continents = ['Europe', 'America', 'Asia', 'Oceania']
     continents_dic = {key: index+1 for index, key in enumerate(continents)}
@@ -10,43 +38,36 @@ def create_continent_data():
     continent_file.close()
     return continents_dic
 
-def get_countries_by_continent(cont):
-    countries = []
-    if cont == 'Europe':
-        countries = [
-            'Austria', 'Belgium', 'Croatia', 'Denmark', 'Finland', 'France', 
-            'Germany', 'Italy', 'Netherlands', 'Norway', 'Spain', 'Sweden', 'UK'
-        ]
-    elif cont == 'America':
-        countries = [
-            'Argentina', 'Brazil', 'Canada', 'Chile', 'Colombia', 'Mexico', 'Uruguay', 'USA', 
-        ]
-    elif cont == 'Asia':
-        countries = [
-            'China', 'India', 'Japan', 'Malaysia', 'Russia', 'Saudi Arabia', 'Singapore',
-            'South Korea', 'Thailand', 'United Arab Emirates', 'Vietnam'
-        ]
-    elif cont == 'Oceania':
-        countries = [
-            'Australia', 'Fiji', 'New Zealand'
-        ]
-    return countries
 
 def create_country_data(continents):
+    print('Creating countries...')
     country_file = open("countries.csv", "w")
     all_countries = {}
-    for key, i in continents.items():
-        countries_dic = {country: index+1 for index, country in enumerate(get_countries_by_continent(key))}
+    for continent, i in continents.items():
+        countries_dic = {country: index+1+len(all_countries) for index, country in enumerate(CONTINENT_COUNTRY[continent])}
         all_countries.update(countries_dic)
         for country, index in countries_dic.items():
             country_file.write('{},{},{}\n'.format(index, country, i))
     country_file.close()
     return all_countries
 
+def create_city_data(countries):
+    print('Creating cities...')
+    city_file = open("cities.csv", "w")
+    all_cities = {}
+    for country, i in countries.items():
+        cities_dic = {city: index+1+len(all_cities) for index, city in enumerate(COUNTRY_CITY[country])}
+        all_cities.update(cities_dic)
+        for city, index in cities_dic.items():
+            city_file.write('{},{},{}\n'.format(index, city, i))
+    city_file.close()
+    return all_cities
+
+
 def main():
     continents = create_continent_data()
     countries = create_country_data(continents)
-    print(countries)
+    cities = create_city_data(countries)
 
 if __name__ == "__main__":
     main()
