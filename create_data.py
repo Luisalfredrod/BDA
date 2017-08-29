62def7ad28f1d0cfa4ef7f78aeee380199f7cb2d
EduardoVaca <vacalalo@gmail.com>
2017-08-29 16:15:50 -0500

    Generating tickets

--- create_data.py
+++ create_data.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import itertools
 import names
 import random
@@ -88,7 +89,7 @@ def create_airport_data(cities):
 
 
 def create_users_data():
-    user_file = open('users.csv', 'w')
+    user_file = open('passengers.csv', 'w')
     num_of_users = int(input('How many users? '))
     print('Creating users...')
     users_id = [x+1 for x in range(num_of_users)]
@@ -139,15 +140,57 @@ def create_routes_data(airports):
     return routesID
 
 
+def create_flight_data(routes, schedules, airplanes_capacity):
+    num_flights = int(input('How many flights? '))    
+    print('Creating flights...')
+    flight_file = open('flights.csv', 'w')
+    flightsID_capacity = {}
+    airplanes = [k for k,_ in airplanes_capacity.items()]
+    for i in range(num_flights):
+        current_date = datetime(2017, random.randint(1, 12), random.randint(1, 28))
+        date_srt = str(current_date.year)+'-'+str(current_date.month)+'-'+str(current_date.day)
+        current_route = routes[random.randint(0, len(routes)-1)]
+        current_schedule = schedules[random.randint(0, len(schedules)-1)]
+        current_airplane = airplanes[random.randint(0, len(airplanes)-1)]
+        current_price, current_ontime = random.randint(100,2000), random.randint(0,1)
+        flight_file.write('{},{},{},{},{},{},{}\n'.format(i+1, current_route, current_schedule, current_airplane, current_price, date_srt, current_ontime))
+        flightsID_capacity[i+1] = airplanes_capacity[current_airplane]
+    flight_file.close()
+    return flightsID_capacity
+
+
+def create_ticket_data(flights_capacity, users):
+    num_tickets = int(input('How many tickets?'))
+    flights_capacity_original = dict(flights_capacity)
+    print('Creating tickets...')
+    ticket_file = open('tickets.csv', 'w')
+    flights = [k for k,_ in flights_capacity.items()]
+    for i in range(num_tickets):
+        current_flight = flights[random.randint(0, len(flights)-1)]
+        flights_capacity[current_flight] -= 1
+        if flights_capacity[current_flight] == 0:
+            flights.remove(current_flight)
+            if len(flights) < 1:
+                return
+        current_user = users[random.randint(0, len(users)-1)]
+        current_date = datetime(2016, random.randint(1, 12), random.randint(1, 28))
+        date_srt = str(current_date.year)+'-'+str(current_date.month)+'-'+str(current_date.day)
+        current_seat = flights_capacity_original[current_flight] - flights_capacity[current_flight]
+        ticket_file.write('{},{},{},{}\n'.format(current_flight, current_user, current_seat, date_srt))
+    ticket_file.close()
+
+
 def main():
     continents = create_continent_data()
     countries = create_country_data(continents)
     cities = create_city_data(countries)
     airports = create_airport_data(cities)
     users = create_users_data()
-    airplanes_capacity = create_airplane_data()
+    airplanes_capacity = create_airplane_data()        
     schedules = create_schedule_data()
     routes = create_routes_data(airports)
+    flights_capacity = create_flight_data(routes, schedules, airplanes_capacity)
+    create_ticket_data(flights_capacity, users)
 
 if __name__ == "__main__":
     main()
