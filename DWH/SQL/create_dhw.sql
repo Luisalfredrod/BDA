c11c46342212eecfaf72e833145f2bae5a94dd6c
EduardoVaca <vacalalo@gmail.com>
2017-10-19 12:56:56 -0500

    Add airport dest id in mv_flight

--- DWH/SQL/create_dhw.sql
+++ DWH/SQL/create_dhw.sql
@@ -107,17 +107,21 @@ BUILD IMMEDIATE
 REFRESH FORCE
 ON DEMAND
 AS
-SELECT ID_FLIGHT, PRICE
-FROM FLIGHT@EUROPE
+SELECT F.id_flight, F.price, R.id_airport_arrival
+FROM FLIGHT@EUROPE F, ROUTE@EUROPE R
+WHERE F.id_route = R.id_route
 UNION
-SELECT ID_FLIGHT, PRICE
-FROM FLIGHT@AMERICA
+SELECT F.id_flight, F.price, R.id_airport_arrival
+FROM FLIGHT@AMERICA F, ROUTE@AMERICA R
+WHERE F.id_route = R.id_route
 UNION
-SELECT ID_FLIGHT, PRICE
-FROM FLIGHT@ASIA
+SELECT F.id_flight, F.price, R.id_airport_arrival
+FROM FLIGHT@ASIA F, ROUTE@ASIA R
+WHERE F.id_route = R.id_route
 UNION
-SELECT ID_FLIGHT, PRICE
-FROM FLIGHT@OCEANIA;
+SELECT F.id_flight, F.price, R.id_airport_arrival
+FROM FLIGHT@OCEANIA F, ROUTE@OCEANIA R
+WHERE F.id_route = R.id_route;
 
 /*
  * Creation of DHW tables.
@@ -126,30 +130,30 @@ FROM FLIGHT@OCEANIA;
 -- Dimension: Destiny table.
 CREATE TABLE D_DESTINY
 (
-id_destiny NUMBER(10) NOT NULL,
-code_airport NUMBER(10) NOT NULL,
-code_city NUMBER(10) NOT NULL,
-code_country NUMBER(10) NOT NULL,
-code_contintent NUMBER(10) NOT NULL,
-airport_name VARCHAR2(50) NOT NULL,
-city_name VARCHAR2(50) NOT NULL,
-country_name VARCHAR2(50) NOT NULL,
-continent_name VARCHAR2(50) NOT NULL,
+    id_destiny NUMBER(10) NOT NULL,
+    code_airport NUMBER(10) NOT NULL,
+    code_city NUMBER(10) NOT NULL,
+    code_country NUMBER(10) NOT NULL,
+    code_contintent NUMBER(10) NOT NULL,
+    airport_name VARCHAR2(50) NOT NULL,
+    city_name VARCHAR2(50) NOT NULL,
+    country_name VARCHAR2(50) NOT NULL,
+    continent_name VARCHAR2(50) NOT NULL,
 
-CONSTRAINT pk_D_Destiny PRIMARY KEY (id_destiny)
+    CONSTRAINT pk_D_Destiny PRIMARY KEY (id_destiny)
 );
 
 -- Dimension: Time table.
 CREATE TABLE D_TIME
 (
-id_time NUMBER(10) NOT NULL,
-date_complete DATE NOT NULL,
-date_year VARCHAR2(4) NOT NULL,
-date_month VARCHAR2(10) NOT NULL,
-date_day NUMBER NOT NULL,
-date_day_name VARCHAR2(10),
+    id_time NUMBER(10) NOT NULL,
+    date_complete DATE NOT NULL,
+    date_year VARCHAR2(4) NOT NULL,
+    date_month VARCHAR2(10) NOT NULL,
+    date_day NUMBER NOT NULL,
+    date_day_name VARCHAR2(10),
 
-CONSTRAINT pk_D_Time PRIMARY KEY (id_time)
+    CONSTRAINT pk_D_Time PRIMARY KEY (id_time)
 );
 
 -- Dimension: Ticket table.
