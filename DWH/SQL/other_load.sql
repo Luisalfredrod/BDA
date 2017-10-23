85f70752f88803c3a95f8dfa81ea234d7573d161
EduardoVaca <vacalalo@gmail.com>
2017-10-23 09:02:10 -0500

    change load

--- DWH/SQL/other_load.sql
+++ DWH/SQL/other_load.sql
@@ -1,24 +1,7 @@
 -- Procedure to load/update dimension table D_DESTINY
 CREATE OR REPLACE PROCEDURE UPDATE_D_DESTINY AS
 BEGIN
-    INSERT INTO D_DESTINY
-    SELECT SEQ_D_DESTINY.NEXTVAL,
-        AIR.id_airport,
-        CI.id_city,
-        COU.id_country,
-        CONT.id_continent,
-        AIR.name,
-        CI.name,
-        COU.name,
-        CONT.name
-    FROM MV_AIRPORT AIR,
-        MV_CITY CI,
-        MV_COUNTRY COU,
-        MV_CONTINENT CONT
-    WHERE AIR.id_city = CI.id_city
-        AND CI.id_country = COU.id_country
-        AND COU.id_continent = CONT.id_continent
-        AND AIR.id_airport NOT IN (SELECT code_airport FROM D_DESTINY);
+    INSERT INTO D_DESTINY SELECT SEQ_D_DESTINY.NEXTVAL, AIR.id_airport, CI.id_city, COU.id_country, CONT.id_continent, AIR.name, CI.name, COU.name, CONT.name FROM MV_AIRPORT AIR, MV_CITY CI, MV_COUNTRY COU, MV_CONTINENT CONT WHERE AIR.id_city = CI.id_city AND CI.id_country = COU.id_country AND COU.id_continent = CONT.id_continent AND AIR.id_airport NOT IN (SELECT code_airport FROM D_DESTINY);
     COMMIT;
 END UPDATE_D_DESTINY;
 /
